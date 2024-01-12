6fcb1628ef1c58c78ef62a1dbde53831980ed27e
sparky4 <sparky4@4ch.irc.su>
2024-01-11 18:14:00 -0600

    HTTPS ENABLED

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'asymmetric_jwt_auth',
+    'certbot_django.server',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
