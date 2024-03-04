c11f03984ee332b53004b458625f700c95df9f19
Austin.Hampton <austin.hampton@ipsgroupinc.com>
2024-03-04 10:01:07 -0600

    Added Coniya's Market
    Has bugs on the market may have something to do with logins
    
    STILL NEED TO COMBINE BOTH LOGINS
    
    BUGGY

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 from .views import UploadView, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView, deleteItem
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('market/', include('core.urls')),
+    path('items/', include('item.urls')),
+    path('inbox/', include('conversation.urls')),
+    path('dashboard/', include('dashboard.urls')),
+    path('admin/', admin.site.urls),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
@@ -14,6 +23,7 @@ urlpatterns = [
     path('list/', views.list, name='list'),
     path('closet/', views.closet, name='user-closet'),
     path('closet/<str:itemid>/delete/', views.deleteItem, name="delete-item")
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 # <app>/<model>_<viewtype>.html
