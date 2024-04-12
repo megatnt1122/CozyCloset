56dff69deef7e4bd65c4f18f7d478f16b196d077
Austin.Hampton <austin.hampton@ipsgroupinc.com>
2024-04-12 11:41:30 -0500

    Added Paypal Integration (Doesn't work due to api conflicts)
    Maybe attempting to replace it so that it visually is appealing.

--- django_project/item/views.py
+++ django_project/item/views.py
@@ -3,7 +3,48 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item, Category
 from .forms import NewItemForm, EditItemForm
 from django.db.models import Q
+import uuid
+from django.urls import reverse
+from paypal.standard.forms import PayPalPaymentsForm
 
+def CheckOut(request, product_id):
+
+    product = Item.objects.get(id=product_id)
+
+    host = request.get_host()
+
+    paypal_checkout = {
+        'business': settings.PAYPAL_RECEIVER_EMAIL,
+        'amount': product.price,
+        'item_name': product.name,
+        'invoice': uuid.uuid4(),
+        'currency_code': 'USD',
+        'notify_url': f"http://{host}{reverse('paypal-ipn')}",
+        'return_url': f"http://{host}{reverse('payment-success', kwargs = {'product_id': product.id})}",
+        'cancel_url': f"http://{host}{reverse('payment-failed', kwargs = {'product_id': product.id})}",
+    }
+
+    paypal_payment = PayPalPaymentsForm(initial=paypal_checkout)
+
+    context = {
+        'product': product,
+        'paypal': paypal_payment
+    }
+
+    return render(request, 'checkout.html', context)
+
+def PaymentSuccessful(request, product_id):
+
+    product = Item.objects.get(id=product_id)
+
+    return render(request, 'payment-success.html', {'product': product})
+
+def paymentFailed(request, product_id):
+
+    product = Item.objects.get(id=product_id)
+
+    return render(request, 'payment-failed.html', {'product': product})
+    
 def items(request):
      query = request.GET.get('query', '')
      category_id = request.GET.get('category', 0)
