aab83de90e34ed84eeb30a015e44fc2141810362
coniya <cbu010@email.latech.edu>
2024-04-23 19:01:09 -0500

    fixed some minor html changes
    looks more cohesive. button colors are changed and prices look like prices

--- django_project/dashboard/views.py
+++ django_project/dashboard/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from item.models import Item
 
-#@login_required
+@login_required
 def index(request):
     items = Item.objects.filter(created_by=request.user)
 
