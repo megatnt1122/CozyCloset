1c52b7db07a7f1b60baf3fa4e4c5e1ce9e61dfbe
sparky4 <sparky4@cock.li>
2024-04-24 14:35:15 -0500

    me and chance's polishing of the form in inbox

--- django_project/blog/forms.py
+++ django_project/blog/forms.py
@@ -22,7 +22,7 @@ class DirectMessagingForm(forms.ModelForm):
         fields = ['content',]
         widgets = {
             'content': forms.Textarea(attrs={
-                'class': 'w-full py-4 px-6 rounded-xl border'
+                'class': 'form-control' #'w-full py-4 px-6 rounded-xl border'
             })
         }
 class CommentForm(forms.ModelForm):
