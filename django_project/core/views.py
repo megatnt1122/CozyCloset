aa35f80c2c0df9d13f8d37a5dcc9643d99854cae
coniya <cbu010@email.latech.edu>
2024-04-29 11:33:06 -0500

    all items are viewable on market homepage

--- django_project/core/views.py
+++ django_project/core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from item.models import Category, Item
 
 def index(request):
-    items = Item.objects.filter(is_sold=False)[0:6]
+    items = Item.objects.filter(is_sold=False).order_by('-id')[:8]
     categories = Category.objects.all()
     return render(request, 'core/index.html', {
         'categories': categories,
